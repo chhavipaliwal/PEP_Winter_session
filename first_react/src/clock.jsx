f8014afa0d3eda7c0de51d0ea2e8b9aec3935de4
chhavi paliwal <chhavipaliwal1203@gmail.com>
2025-01-18 16:46:30 +0530

    Refactor Clock and Login components by improved clarity and maintainability

--- first_react/src/clock.jsx
+++ first_react/src/clock.jsx
@@ -1,48 +1,13 @@
-// import React, { useState, useEffect } from "react";
-
-// function DigitalClock() {
-//   const [time, setTime] = useState(new Date());
-
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       setTime(new Date());
-//     }, 1000);
-
-//     return () => clearInterval(timer); // Cleanup timer
-//   }, []);
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         flexDirection: "column",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         height: "100vh",
-//         fontFamily: "Arial, sans-serif",
-//         fontSize: "3rem",
-//         backgroundColor: "#282c34",
-//         color: "white",
-//       }}
-//     >
-//       <p>{time.toLocaleTimeString()}</p>
-//     </div>
-//   );
-// }
-// export default DigitalClock;
 import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
-
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
-
     return () => clearInterval(timer); // Cleanup timer
   }, []);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen font-sans text-3xl bg-[#282c34] text-white">
       <p>{time.toLocaleTimeString()}</p>
