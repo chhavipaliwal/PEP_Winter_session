b6dc8c2e66df6d991f36b17143c92d308eacb71f
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-01-21 15:41:45 +0530

    Implement callback function to display employee details and log success message

--- third_react/callback.js
+++ third_react/callback.js
@@ -4,16 +4,11 @@
 //     console.log("Hello, " + name + "!");
 //     callback(); // Call the callback function
 //   }
-
-//   // Define the callback function
 //   function sayGoodbye() {
 //     console.log("Goodbye! Have a great day!");
 //   }
-
-//   // Use the greetUser function with sayGoodbye as the callback
 //   greetUser("John", sayGoodbye);
 // }
-
 function myfun(val) {
   console.log(val(), "Hello, world!");
 }
--- third_react/secenerio.js
+++ third_react/secenerio.js
@@ -1,3 +1,20 @@
 //a scenerio where i want to display the employee display employee no, emlpoyeename, employee salary,
 // employee department using callback function
 //here is a code of callback function
+function display(employee, callback) {
+  console.log("Employee no: " + employee.no);
+  console.log("Employee name: " + employee.name);
+  console.log("Employee salary: " + employee.salary);
+  console.log("Employee department: " + employee.department);
+  callback();
+}
+function displayEmployee() {
+  console.log("Employee details displayed successfully");
+}
+const employee = {
+  no: 101,
+  name: "Sai",
+  salary: 20000,
+  department: "IT",
+};
+display(employee, displayEmployee);
